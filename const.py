797ad54a0462685d3d4f25f458580b5d32a78929
Himanshu Gupta <himgupta1996@gmail.com>
2021-03-27 20:20:37 -0400

    Editing Constant file containing the server port IDs.

--- const.py
+++ const.py
@@ -1,3 +1,3 @@
 FRONTEND_SERVER = {"IP": "http://127.0.0.1", "PORT": 8010}
-CLIENT_SERVER = {"IP": "http://127.0.0.1", "PORT": 8011}
+CATALOG_SERVER = {"IP": "http://127.0.0.1", "PORT": 8011}
 ORDER_SERVER = {"IP": "http://127.0.0.1", "PORT": 8012}
